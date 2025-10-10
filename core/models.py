57efb1d0983052cfff25f27617beedd5d75772b5
Олег Филипенков <olegfilipenkov@MacBook-Pro-Oleg.local>
2025-10-10 18:14:39 +0300

    create pydantic classes AbstractEmployee, CreditCard

--- core/models.py
+++ core/models.py
@@ -2,20 +2,33 @@ from datetime import date
 
 from phonenumbers.phonenumber import PhoneNumber
 from pydantic import BaseModel, EmailStr
+from pydantic_extra_types.payment import PaymentCardNumber, PaymentCardBrand
 
-
-class AbstractPerson(BaseModel):
+class ConfigModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
+class CreditCard(ConfigModel):
+    credit_number: PaymentCardNumber
+    type_credit_card: str
+    date_expired: date
+    card_brand: PaymentCardBrand
+
+
+class AbstractPerson(ConfigModel):
+
     id: int
     first_name: str
+    first_name_county_lang: str
     second_name: str
+    second_name_county_lang: str
     third_name: str
-    email: EmailStr
-    phone_number: PhoneNumber
+    third_name_county_lang: str
+    email_address: EmailStr
     address: str
     birthdate: date
+    phone_number: PhoneNumber
+    credit_card: CreditCard
 
     def __str__(self):
         return f"""
@@ -24,3 +37,16 @@ class AbstractPerson(BaseModel):
         \nbirthdate: {self.birthdate}
         
 """
+
+class AbstractEmployee(AbstractPerson):
+
+    working_address: str
+    working_email_address: EmailStr
+    working_phone_number: PhoneNumber
+    working_credit_card: CreditCard
+
+
+
+
+
+
