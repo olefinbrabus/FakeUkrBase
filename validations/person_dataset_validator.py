f37b04bc8614ea61984298d4e4a24a1b8089a65d
Олег Филипенков <olegfilipenkov@MacBook-Pro-Oleg.local>
2025-10-23 22:53:34 +0300

    chagne validate logic

--- validations/person_dataset_validator.py
+++ validations/person_dataset_validator.py
@@ -6,29 +6,30 @@ from core import AbstractPerson, AbstractEmployee
 
 def validate_person_dataset(dataset: DataFrame, person_class: AbstractPerson = AbstractPerson):
     copied_dataset = dataset.copy(deep=True)
-    attributes_to_check_is_repeat: list[str] = []
+    attributes_to_check_is_have_duplicate: list[str] = []
+    duplicate_list_by_categories: list[dict] = []
+
+
 
     if isinstance(person_class, AbstractPerson):
-        attributes_to_check_is_repeat = [
+        attributes_to_check_is_have_duplicate = [
             "email_address", "phone_number", "id", "credit_card"
         ]
     elif isinstance(person_class, AbstractEmployee):
-        attributes_to_check_is_repeat += [
+        attributes_to_check_is_have_duplicate += [
             "working_email_address", "work_phone_number", "working_credit_card"
         ]
     else:
         raise TypeError("person_class must be an instance of AbstractPerson")
 
-    for attribute in attributes_to_check_is_repeat:
-        index_duplicate_items = validate_repeatable_items_in_dataset_by_column(copied_dataset, attribute)
+    for attribute in attributes_to_check_is_have_duplicate:
+        index_duplicate_items = validate_duplicate_items_in_dataset_by_column(copied_dataset, attribute)
+        if len(index_duplicate_items) > 1:
+            duplicate_list_by_categories.append(index_duplicate_items)
 
+    return duplicate_list_by_categories
 
-
-
-
-
-
-def validate_repeatable_items_in_dataset_by_column(dataset: DataFrame, column: str):
+def validate_duplicate_items_in_dataset_by_column(dataset: DataFrame, column: str):
     item_list = dataset[column].values.tolist()
     print(item_list)
 
