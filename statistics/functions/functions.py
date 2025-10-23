18bc9545b69625d6a21913822c89136a551a29d3
Олег Филипенков <olegfilipenkov@MacBook-Pro-Oleg.local>
2025-10-23 17:38:14 +0300

    add validator for dataframe

--- statistics/functions/functions.py
+++ statistics/functions/functions.py
@@ -1,9 +1,11 @@
+from pandas import DataFrame
+
 from config import UKRAINIAN_OPERATORS
 from core import AbstractPerson
 
 
-def get_phone_operators_count(self) -> dict[str, int]:
-    frame = self.dataframe["phone number"]
+def get_phone_operators_count(dataframe: DataFrame) -> dict[str, int]:
+    frame = dataframe["phone number"]
     phone_operators_count = {keys: 0 for keys in UKRAINIAN_OPERATORS.keys()}
     for number in frame:
         operator = AbstractPerson.phone_operator(number)
--- validations/person_dataset_validator.py
+++ validations/person_dataset_validator.py
@@ -1,9 +1,53 @@
+from PIL.DdsImagePlugin import item1
 from pandas import DataFrame
 
-
-def validate_person_dataset(dataset: DataFrame):
-    copied_dataset = dataset.copy()
+from core import AbstractPerson, AbstractEmployee
 
 
-def validate_repeatable_words_in_dataset_by_column(dataset: DataFrame, column: str):
-    pass
+def validate_person_dataset(dataset: DataFrame, person_class: AbstractPerson = AbstractPerson):
+    copied_dataset = dataset.copy(deep=True)
+    attributes_to_check_is_repeat: list[str] = []
+
+    if isinstance(person_class, AbstractPerson):
+        attributes_to_check_is_repeat = [
+            "email_address", "phone_number", "id", "credit_card"
+        ]
+    elif isinstance(person_class, AbstractEmployee):
+        attributes_to_check_is_repeat += [
+            "working_email_address", "work_phone_number", "working_credit_card"
+        ]
+    else:
+        raise TypeError("person_class must be an instance of AbstractPerson")
+
+    for attribute in attributes_to_check_is_repeat:
+        index_duplicate_items = validate_repeatable_items_in_dataset_by_column(copied_dataset, attribute)
+
+
+
+
+
+
+
+def validate_repeatable_items_in_dataset_by_column(dataset: DataFrame, column: str):
+    item_list = dataset[column].values.tolist()
+    print(item_list)
+
+    duplicates: dict = {"_specific_word": column}
+    for i, item in enumerate(item_list):
+        if item_list.count(item) > 1:
+            if item not in duplicates:
+                duplicates[item] = [i]
+            else:
+                duplicates[item].append(i)
+
+    return duplicates
+
+
+
+
+
+# df = DataFrame({
+#     "numbers": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 1, 15],
+# })
+#
+# print(validate_repeatable_items_in_dataset_by_column(df, "numbers"))
