aae5af2d8dda0a0010d663b5040fd421f6384015
Oleg <olefinibrabus@gmail.com>
2025-12-16 22:49:45 +0200

    add employee validation

--- validations/employee_validations/employee_validator.py
+++ validations/employee_validations/employee_validator.py
@@ -1,3 +1,18 @@
+from core import AbstractEmployee
+from validations.job_validations.job_validator import validate_job
 
-def validate_employee():
-    pass
+
+def validate_employee(employee: AbstractEmployee):
+    if not validate_job(employee.job):
+        print('Employee Job not valid')
+        return False
+
+    if employee.length_of_work > 55:
+        print('Employee Length of Work is greater than 55 years')
+        return False
+
+    if employee.contract_payment<=0:
+        print('Employee Contract Payment cant\'t be less than 0')
+        return False
+
+    return True
--- validations/validator.py
+++ validations/validator.py
@@ -2,13 +2,14 @@ from pandas import DataFrame
 from progress.bar import ShadyBar
 from pydantic import ValidationError
 
-from core import AbstractPerson
+from core import AbstractPerson, AbstractEmployee
 from validations.abstract_person_validations.abstract_person_validator import (
     validate_abstract_person,
 )
 from validations.dataset_validations.person_dataset_validator import (
     validate_person_categories,
 )
+from validations.employee_validations.employee_validator import validate_employee
 
 
 def validate_persons(
@@ -21,8 +22,12 @@ def validate_persons(
         if not validate_abstract_person(person, person_cls):
             print(f"{person} is not a valid")
             raise Exception
+        if isinstance(person, AbstractEmployee) and not validate_employee(person):
+                print(f"{person} is not a valid")
+                raise Exception
         bar.next()
     bar.finish()
+
 
 
 def validate_person_dataset(
